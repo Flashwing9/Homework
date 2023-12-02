095715f1fcbf3b743c4e72062ffc944864f4f45a
Konstantin <114243664+Flashwing9@users.noreply.github.com>
2023-12-02 22:17:37 +0100

    js first homework

---
+++ 8 workingWithJSVariables/app.js
@@ -0,0 +1,5 @@
+const price1Phone = 119.95;
+//there are 30 phones
+let tax = (5 * price1Phone) / 100;
+
+console.log("30 phones cost", price1Phone * 30, "\n", "With applied tax of 5%, 30 phones cost", tax + (price1Phone * 30));
