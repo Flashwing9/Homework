d44ccb8420ffd031edff95e9fea7e0878585110d
Konstantin <114243664+Flashwing9@users.noreply.github.com>
2023-12-04 12:26:17 +0100

    a

--- JavaScript/8 workingWithJSVariables/app.js
+++ JavaScript/8 workingWithJSVariables/app.js
@@ -1,5 +1,6 @@
-const price1Phone = 119.95;
-//there are 30 phones
+// Write a JavaScript program that will calculate the price of 30 Phones, where the price of one phone is $119.95 and the tax rate is 5%.
+
+let price1Phone = 119.95 * 30;
 let tax = (5 * price1Phone) / 100;
 
-console.log("30 phones cost", price1Phone * 30, "\n", "With applied tax of 5%, 30 phones cost", tax + (price1Phone * 30));
+console.log("30 phones cost", price1Phone, "\n", "With applied tax of 5%, 30 phones cost", tax + price1Phone);
