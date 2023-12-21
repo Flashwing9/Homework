b3b20f27a764dcd37dda9493ebc3820ff677aafd
Konstantin <114243664+Flashwing9@users.noreply.github.com>
2023-12-21 16:17:00 +0100

    started bonus hw for class05

---
+++ JavaScript/class05/3 bonusRecipePage/app.js
@@ -0,0 +1,20 @@
+// BONUS HOMEWORK
+// Create a recipe page from inputs
+
+// Ask the user for the name of the recipe ✅
+// Ask the user how many ingredients do we need for the recipe ✅
+// Ask the user for the name of every ingredient
+// Print the name of the recipe in the HTML as heading element, ex: h1-h6
+// Print all ingredients as an unordered list in the HTML
+// Extra: Use a table if you want to be fancy :)
+
+
+
+const nameRecipeInput = prompt("What recipe will you make?");
+const amountOfIngredients = prompt("How many ingredients will you need?");
+
+function storingIngredientNames() {
+    let ingredientArr = [nameRecipeInput];
+}
+
+const printRecipeName = document.querySelector(".recipeName").textContent = nameRecipeInput;
